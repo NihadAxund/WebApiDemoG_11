fb0d001b8daa40cfc6a6b3c5b86b2571f573b116
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-05-18 18:17:53 +0400

    Add project files.

---
+++ WebApiDemoG/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApiDemoG.Models;
+
+namespace WebApiDemoG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpGet("")]
+        public List<ContactModel> Get()
+        {
+            return new List<ContactModel>
+            {
+                new ContactModel{Id=1,Firstname="Tural",Lastname="Eliyev",PAN="4169855878789955",Age=25},
+                new ContactModel{Id=2,Firstname="Mike",Lastname="Sina",PAN="4169996678789955",Age=35},
+                new ContactModel{Id=1,Firstname="Tural",Lastname="Eliyev",PAN="4169855878789955",Age=25},
+                new ContactModel{Id=2,Firstname="Mike",Lastname="Sina",PAN="4169996678789955",Age=35}
+            };
+        }
+
+        [HttpGet("First")]
+        public ContactModel GetFirst()
+        {
+            return new ContactModel { Id = 1, Firstname = "Tural", Lastname = "Eliyev", PAN = "4169855878789955",Age = 25 };
+        }
+    }
+}
---
+++ WebApiDemoG/Formatters/VCardOutputFormatter.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using WebApiDemoG.Models;
+
+namespace WebApiDemoG.Formatters
+{
+    public class VCardOutputFormatter : TextOutputFormatter
+    {
+        public VCardOutputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/vcard"));
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var response=context.HttpContext.Response;
+            var stringBuilder=new StringBuilder();
+            if(context.Object is List<ContactModel> list)
+            {
+                foreach (var model in list)
+                {
+                    FormatVCard(stringBuilder, model);
+                }
+            }
+            else
+            {
+                var contact = context.Object as ContactModel;
+                FormatVCard(stringBuilder, contact);
+            }
+            return response.WriteAsync(stringBuilder.ToString());
+        }
+
+        private static void FormatVCard(StringBuilder builder,ContactModel model)
+        {
+            builder.AppendLine("BEGIN:VCARD");
+            builder.AppendLine("VERSION:2.1");
+            builder.AppendLine($"FN:{model.Firstname}");
+            builder.AppendLine($"LN:{model.Lastname}");
+            builder.AppendLine($"PAN:{model.PAN}");
+            builder.AppendLine($"UID:{model.Id}");
+            builder.AppendLine("END:VCARD");
+        }
+
+        protected override bool CanWriteType(Type? type)
+        {
+            if(typeof(ContactModel).IsAssignableFrom(type) ||
+                typeof(List<ContactModel>).IsAssignableFrom(type))
+            {
+                return base.CanWriteType(type);
+            }
+            return false;
+        }
+    }
+}
